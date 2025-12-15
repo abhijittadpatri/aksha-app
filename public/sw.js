b3303017864086e8265a7bad62f71e07672a5794
Tadpatri Abhijit <abhijittadpatri@outlook.com>
2025-12-16 00:45:58 +0530

    PWA: respondWith pass-through fetch for desktop installability

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,5 @@ self.addEventListener("activate", (event) => {
 // Minimal fetch handler to satisfy PWA installability on desktop.
 // We are NOT caching API responses yet (safer for Week 0).
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
   // Default: just pass-through
 });
